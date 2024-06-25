637e4bdcd3b74d164738faca075af7c0f35ba4a1
hieu <hirosaki217@gmail.com>
2024-06-25 22:51:52 +0700

    splash screen

--- src/screens/SplashScreen.tsx
+++ src/screens/SplashScreen.tsx
@@ -1,11 +1,38 @@
-import {View, Text} from 'react-native';
+import {
+  View,
+  Text,
+  ImageBackground,
+  Image,
+  ActivityIndicator,
+} from 'react-native';
 import React from 'react';
+import {appInfo} from '../constants/appInfos';
+import {SpaceComponent} from '../components';
+import {appColors} from '../constants/appColors';
 
 const SplashScreen = () => {
   return (
-    <View>
-      <Text>SplashScreen</Text>
-    </View>
+    <ImageBackground
+      source={require('../assets/images/splash-img.png')}
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignContent: 'center',
+      }}
+      imageStyle={{
+        flex: 1,
+      }}>
+      <Image
+        source={require('../assets/images/logo.png')}
+        style={{
+          width: appInfo.sizes.WIDTH * 0.7,
+          resizeMode: 'contain',
+          alignSelf: 'center',
+        }}
+      />
+      <SpaceComponent height={15} />
+      <ActivityIndicator color={appColors.gray} size={25} />
+    </ImageBackground>
   );
 };
 
