d79745f0bf979afd0daf65d2f39c627c859af407
hieu <hirosaki217@gmail.com>
2024-06-25 22:19:56 +0700

    first commit

---
+++ src/navigators/TabNavigator.tsx
@@ -0,0 +1,17 @@
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import React from 'react';
+import {HomeScreen} from '../screens';
+
+const TabNavigator = () => {
+  const Tab = createBottomTabNavigator();
+  return (
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Tab.Screen name="HomeScreen" component={HomeScreen}></Tab.Screen>
+    </Tab.Navigator>
+  );
+};
+
+export default TabNavigator;
